20a0bb0f887e8609cff68186c7b7a569c1179865
bman <>
2026-02-17 11:32:24 -0800

    test

---
+++ Test101.java
@@ -0,0 +1,15 @@
+package cit63;
+
+import java.util.Scanner;
+
+public class Test101 {
+
+public static void main(String[] args) {
+		
+		Scanner stdin = new Scanner( System.in ); //Scanner
+		
+		System.out.println("YES");
+		
+	
+}
+}
